cdc1163ac4c2684aa65528317b9a410263d2f0d5
pdevouge <paul.lucas.devouge@cern.ch>
2026-06-19 21:54:03 +0200

    High mass model to use the correct PDF objects

--- python/HighMass.py
+++ python/HighMass.py
@@ -6,13 +6,14 @@ from HiggsAnalysis.CombinedLimit.PhysicsModel import *
 
 class HighMassModel(PhysicsModelBase):
     """High mass Xgg model"""
+    # Yield = u*S+sqrt(u)*I+B = sqrt(u)*(S+Bggf+I)+(u-sqrt(u))*S-sqrt(u)*Bggf+Btot
 
     def doParametersOfInterest(self):
         """Create POI and other parameters, and define the POI set."""
         self.modelBuilder.doVar("mu[1,0,20]")
         # --- S+B+I ---#
         self.modelBuilder.factory_('expr::scale_S("@0-sqrt(@0)", mu)')
-        self.modelBuilder.factory_('expr::scale_B("1-sqrt(@0)", mu)')
+        self.modelBuilder.factory_('expr::scale_B("-sqrt(@0)", mu)')
         self.modelBuilder.factory_('expr::scale_SBI("sqrt(@0)", mu)')
         self.modelBuilder.doSet("POI", "mu")
         # --- Higgs Mass as other parameter ----
@@ -24,13 +25,12 @@ class HighMassModel(PhysicsModelBase):
                 self.modelBuilder.doVar("MH[%g]" % self.options.mass)
 
     def getYieldScale(self, bin, process):
-        "Return the name of a RooAbsReal to scale this yield by or the two special values 1 and 0 (don't scale, and set to zero)"
         if not self.DC.isSignal[process]:
+            if process == "sbi_mass":
+                return "scale_SBI"
+            elif process == "ggbox_mass"
+                return "scale_B"
             return 1
-        pname = process.lower()
-        if 'rsg' or 'spin0' in pname:
-            return "scale_S"
-        if 'interference' in pname:
-            return "scale_SBI"
-        if 'ggbox' in pname:
-            return "scale_B"
+        return "scale_S"
+
+highMass = HighMassModel()
